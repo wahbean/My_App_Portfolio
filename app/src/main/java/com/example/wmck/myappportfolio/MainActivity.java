4e5fc8f91650f9c94472ab8310253b6aab06baa6
wahbean <wahbean@users.noreply.github.com>
2015-06-17 12:07:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/wmck/myappportfolio/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.wmck.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Button spotifyButton = (Button) findViewById(R.id.spotify);
+        spotifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This will start the Spotify App", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Button scoresButton = (Button) findViewById(R.id.scores);
+        scoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This button with launch the Scores App", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button libraryButton = (Button) findViewById(R.id.library);
+        libraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This button with launch the Library App", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button biggerButton = (Button) findViewById(R.id.bigger);
+        biggerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This button with launch the Bigger Built App", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button xyzButton = (Button) findViewById(R.id.xyz);
+        xyzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This button with launch the XYZ App", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button myButton = (Button) findViewById(R.id.myOwn);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This button with launch my very own capstone app!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
